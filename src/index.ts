3f5b048e39b91ee205cff361dcdf4b5db6f9d2f1
Will Neville <willnevillain@gmail.com>
2024-04-09 16:21:22 -0400

    feat: Add base example for exposing endpoint and fetching from external API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+/* eslint-disable no-console */
+import express from 'express';
+import fetch from 'make-fetch-happen';
+
+const PORT = 3000;
+
 const main = () => {
-  // eslint-disable-next-line no-console
-  console.log('Hello world!');
+  const app = express();
+
+  app.get('/', (_, res) => {
+    res.send('Hello World!');
+  });
+
+  app.get('/china', async (_, res) => {
+    const data = await fetch('https://restcountries.com/v3.1/name/china').then(
+      (res) => res.json(),
+    );
+    console.log(JSON.stringify(data));
+    res.send(data);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
 };
 
 void main();
